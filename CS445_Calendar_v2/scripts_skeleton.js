3a3e528e5702cd958fe200d00c23c7f19dfa7c69
Trevor Zobrist <77289395+tzobrist@users.noreply.github.com>
2023-04-25 15:59:54 -0500

    Calendar v2

---
+++ CS445_Calendar_v2/scripts_skeleton.js
@@ -0,0 +1,30 @@
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+// move to next month
+function next() {
+}
+
+//move to previous month
+function previous() {
+}
+
+//jump to selected month and/or year
+function jump() {
+}
+
+//display calendar
+function showCalendar(month, year) {
+}
+
+//check username and password against valid usernames and passwords
+function login() {
+}
